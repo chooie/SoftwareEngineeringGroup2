c15a9ff59aa8e409eaaec59242b0b38297e29b6a
Charlie Hebert <charlie.hebert92@gmail.com>
2015-02-07 19:01:37 +0000

    Add original changes

--- CurrencySuiteWeb/App/Home/Home.js
+++ CurrencySuiteWeb/App/Home/Home.js
@@ -3,25 +3,106 @@
 (function () {
     "use strict";
 
-    // The initialize function must be run each time a new page is loaded
-    Office.initialize = function (reason) {
+    // Handler that is executed when the app is loaded
+    Office.initialize = function () {
         $(document).ready(function () {
             app.initialize();
-
-            $('#get-data-from-selection').click(getDataFromSelection);
+            // Initialize the swap button click listener
+            $('#swap').click(swap);
+            // Initialize the submit button click listener
+            $('#submit').click(convert);
         });
     };
 
-    // Reads data from current document selection and displays a notification
-    function getDataFromSelection() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    app.showNotification('The selected text is:', '"' + result.value + '"');
-                } else {
-                    app.showNotification('Error:', result.error.message);
-                }
-            }
+    /**
+     * Swaps the selected value in the drop-down
+     */
+    var swap = function () {
+        //gets indexes
+        var from = $("#selectedFromCur option:selected").index() + 1;
+        var to = $("#selectedToCur option:selected").index() + 1;
+
+        //remove the current selections
+        $("#selectedToCur :nth-child(" + to + ")").removeAttr('selected');
+        $("#selectedFromCur:nth-child(" + from + ")").removeAttr('selected');
+
+        //assign the new selections
+        $("#selectedFromCur :nth-child(" + to + ")").prop('selected', true);
+        $("#selectedToCur :nth-child(" + from + ")").prop('selected', true);
+    };
+
+    /**
+     * Multiplies a number by some exchange rate value.
+     * @param {number} value The initial number
+     * @returns {number} The converted number
+     */
+    var convertValue = function (value) {
+        if (typeof value !== "number") {
+            app.showNotification("Error", "One of the values is not a number.");
+            // @Max&Thomas, Not sure how we should handle exceptions yet. I'll
+            // look into it.
+            throw new Error("convertValue(): parameter [0] is not a number type");
+        }
+        // Later on use the real conversion rate
+        return value * 2;
+    };
+
+    /**
+     * Convert?
+     */
+    var convert = function () {
+        // Retrieves values from excel
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Matrix,
+          // Some optional parameters
+          {
+              valueFormat: Office.ValueFormat.Unformatted,
+              filterType: Office.FilterType.All
+          },
+          // The callback function
+          function (asyncResult) {
+              if (asyncResult.status == "failed") {
+                  app.showNotification("Failed", asyncResult.error.message);
+                  return;
+              }
+              // Hide error messages
+              $('#notification-message').hide();
+
+              // Gets current date (currently unused for anything)
+              //var currentDate = new Date();
+              //var datetime = "Last Sync: " + currentDate.getDate() + "/"
+              //  + (currentDate.getMonth() + 1) + "/"
+              //  + currentDate.getFullYear() + " @ "
+              //  + currentDate.getHours() + ":"
+              //  + currentDate.getMinutes() + ":"
+              //  + currentDate.getSeconds();
+
+              // .val is the value attribute, it is converted into a base 10 int
+              //var fromCur = parseFloat($('#selectedFromCur').val());
+              //var toCur = parseFloat($('#selectedToCur').val());
+
+              // If only one cell is given (for some s***** reason it changes to
+              // a number type if only one cell is selected, screw consistency...)
+              if (typeof asyncResult.value == "number") {
+                  asyncResult.value = convertValue(asyncResult.value);
+              }
+
+              for (var i = 0; i < asyncResult.value.length; i++) {
+                  for (var j = 0; j < asyncResult.value[i].length; j++) {
+                      // Uses value of two currencies to get a result, returns NaN if
+                      // can't parse
+                      var cell = convertValue(parseFloat(asyncResult.value[i][j]));
+                      // NaN checks if a string isn't in a number format
+                      if (!isNaN(cell)) {
+                          // If it is a number then
+                          asyncResult.value[i][j] = cell;
+                      }
+                  }
+              }
+              Office.context.document.setSelectedDataAsync(
+                // Inserts a (possible) 2d array of values back into excel
+                asyncResult.value
+              );
+          } // end of callback
         );
-    }
+    };
 })();
