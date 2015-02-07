fd97a891119921dc61569bc90a63cb7cb6a5b2c4
Charlie Hebert <charlie.hebert92@gmail.com>
2015-02-07 22:45:34 +0000

    Improve feedback to users when converting

--- CurrencySuiteWeb/App/Home/Home.js
+++ CurrencySuiteWeb/App/Home/Home.js
@@ -36,18 +36,31 @@
      * @param {number} value The initial number
      * @returns {number} The converted number
      */
-    var convertValue = function (value) {
-        if (isNaN(value)) {
-           //if it isn't a number
-            return value;
+    var convertValue = function (value, exchangeRate) {
+        try {
+            if (typeof value !== "number" || isNaN(value)) {
+                throw new Error("convertValue(): parameter [0] is not of the number type. " +
+                    typeof value + " given (" + value + ").");
+            }
+
+            if (typeof exchangeRate !== "number" || isNaN(value)) {
+                throw new Error("convertValue(): parameter [1] is not of the number type. " +
+                    typeof exchangeRate + " given (" + exchangeRate + ").");
+            }
+            // Later on use the real conversion rate
+            // TODO
+            return value * exchangeRate;
+        } catch (error) {
+            console.log(error.message);
+            app.showNotification("Warning", "Some data could not be converted as it " +
+                "was not a valid number.");
         }
-        // Later on use the real conversion rate
-        return value * 2;
     };
 
     /**
      * Convert?
      */
+    var original;
     var convert = function () {
         // Retrieves values from excel
         Office.context.document.getSelectedDataAsync(Office.CoercionType.Matrix,
@@ -58,6 +71,20 @@
           },
           // The callback function
           function (asyncResult) {
+              // Temporary way of better informing the user of when they need to re-select
+              // the data
+              if (asyncResult.value.length === 1 && asyncResult.value[0][0] === original) {
+                  app.showNotification("Whoops", "You gave us the same value as last time. " +
+                      "Try re-selecting the cell.");
+                  return;
+              }
+              original = asyncResult.value[0][0];
+              // User is attempting to use convert whilst still inputting into the cell
+              if (asyncResult.value.length === 1 && asyncResult.value[0][0] === "") {
+                  app.showNotification("Try Again", "Please re-select the data as it " + 
+                      "has not been properly selected");
+                  return;
+              }
               if (asyncResult.status == "failed") {
                   app.showNotification("Failed", asyncResult.error.message);
                   return;
@@ -81,14 +108,14 @@
               // If only one cell is given (for some s***** reason it changes to
               // a number type if only one cell is selected, screw consistency...)
               if (typeof asyncResult.value == "number") {
-                  asyncResult.value = convertValue(asyncResult.value);
+                  asyncResult.value = convertValue(parseFloat(asyncResult.value), 2);
               }
 
               for (var i = 0; i < asyncResult.value.length; i++) {
                   for (var j = 0; j < asyncResult.value[i].length; j++) {
                       // Uses value of two currencies to get a result, returns NaN if
                       // can't parse
-                      var cell = convertValue(parseFloat(asyncResult.value[i][j]));
+                      var cell = convertValue(parseFloat(asyncResult.value[i][j]), 2);
                       // NaN checks if a string isn't in a number format
                       if (!isNaN(cell)) {
                           // If it is a number then
@@ -100,6 +127,9 @@
                 // Inserts a (possible) 2d array of values back into excel
                 asyncResult.value
               );
+
+              app.showNotification("Success", "Your currencies have successfully " +
+                  "been converted!");
           } // end of callback
         );
     };
