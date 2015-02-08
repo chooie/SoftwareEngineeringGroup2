87b1e2fe7d43fa113c57435e6984a26af3bce6f6
finneym <finnemy@tcd.ie>
2015-02-08 20:37:09 +0000

    made a junk.js file for any extras which might be handy but aren't used anymore. Also made tomas's regex slightly stricter and explained what's happening incase anyone doesn't know regex. Also added in the scrappers exhangerate variable again so it's using real values.

--- CurrencySuiteWeb/App/Home/Home.js
+++ CurrencySuiteWeb/App/Home/Home.js
@@ -29,15 +29,7 @@
         currentExchangeRate = 1; //a nice safe value
         scrapper.initialize(); //initalize the scrapper
 
-        //Unused but may be used later for reverting if scrapper fails
-        //oldFrom = $('#selectedFromCur option:selected').index() + 1;
-        //oldTo = $('#selectedToCur option:selected').index() + 1;
-        //$('#selectedFromCur').focus(function () {
-        //    oldFrom = $('#selectedFromCur').index() + 1;
-        //});
-        //$('#selectedToCur').focus(function () {
-        //    oldFrom = $('#selectedToCur').index() + 1;
-        //});
+
         //update with the default rate
         scrapper.updateRate(
             $('#selectedFromCur').val(),
@@ -91,35 +83,31 @@
      */
     var convertValue = function (value) {
         try {
-            if (/\d+\s+\D{3}\s+\D{3}/.test(value)) {
+            //^\d = must start with digit, \d = one or more digits, \.? = zero or one decimal point, \d* = zero or more digits
+            // \s+ = one or more spaces, \D{3} = 3 non-digits, \D{3}$ = 3 non-digits, $ means it must end with this
+            if (/^\d+\.?\d*\s+\D{3}\s+\D{3}$/.test(value)) {
                 var valueArray = value.split(" ");
                 // return will look something like;
                 // return valueArray[0] * getExchangeRate(value[1], value[2]);
+                //*NOTE* We're going to have to have a delay or something so the API/scrapper can get the exchange rates
                 return valueArray[0] * 10;
             }
-            else if ((/\d+/.test(value))) {
+            else if ((/^\d+\.?\d*$/.test(value))) {
                 // return will look something like;
                 // return valueArray * getExchangeRate(dropDownSelectionOne, dropDownSelectionTwo);
-                return value * 2;
+                return value * currentExchangeRate;
             }
             else {
                throw new Error("convertValue(): parameters are wrong. " +
                     typeof value + " given (" + value + ").");
             }
-            //if (typeof value !== "number" || isNaN(value)) {
-            //   throw new Error("convertValue(): parameter [0] is not of the number type. " +
-            //        typeof value + " given (" + value + ").");
-            // }
-
-            //if (typeof currentExchangeRate !== "number" || isNaN(value)) {
-             //   throw new Error("convertValue(): currentExchangeRate is not of the number type. " +
-             //       typeof currentExchangeRate + " given (" + currentExchangeRate + ").");
-            //app.showNotification(value
+            
         } catch (error) {
             console.log(error.message);
             errorOccurred = true;
             app.showNotification("Warning", "Some data could not be converted as it " +
                 "was not a valid number.");
+            return value;
         }
 
     };
@@ -144,12 +132,13 @@
               }
               // Temporary way of better informing the user of when they need to re-select
               // the data
-              if (asyncResult.value.length === 1 && asyncResult.value[0][0] === original) {
-                  app.showNotification("Whoops", "You gave us the same value as last time. " +
-                      "Try re-selecting the cell.");
-                  errorOccurred = true;
-                  return;
-              }
+              //disabled this if statement since it's annoying to debug with, uncomment it if you like
+              //if (asyncResult.value.length === 1 && asyncResult.value[0][0] === original) {
+              //    app.showNotification("Whoops", "You gave us the same value as last time. " +
+              //        "Try re-selecting the cell.");
+              //    errorOccurred = true;
+              //    return;
+              //}
               original = asyncResult.value[0][0];
               // User is attempting to use convert whilst still inputting into the cell
               if (asyncResult.value.length === 1 && asyncResult.value[0][0] === "") {
@@ -158,27 +147,8 @@
                   errorOccurred = true;
                   return;
               }
-              
 
-              // Gets current date (currently unused for anything)
-              //var currentDate = new Date();
-              //var datetime = "Last Sync: " + currentDate.getDate() + "/"
-              //  + (currentDate.getMonth() + 1) + "/"
-              //  + currentDate.getFullYear() + " @ "
-              //  + currentDate.getHours() + ":"
-              //  + currentDate.getMinutes() + ":"
-              //  + currentDate.getSeconds();
-
-              // .val is the value attribute, it is converted into a base 10 int
-              //var fromCur = parseFloat($('#selectedFromCur').val());
-              //var toCur = parseFloat($('#selectedToCur').val());
-
-              // If only one cell is given (for some s***** reason it changes to
-              // a number type if only one cell is selected, screw consistency
-              
-             //if (typeof asyncResult.value == "number") {
-             //     asyncResult.value = convertValue(parseFloat(asyncResult.value), 2);
-             //}
+             
               // for each cell selected, calculate the new values
               // and prepare the insertion array
               for (var i = 0; i < asyncResult.value.length; i++) {
---
+++ CurrencySuiteWeb/App/junk.js
@@ -0,0 +1,45 @@
+﻿//this is for code that is currently unused but may be used in the future
+
+//-------------------------------------------------------------
+
+//Unused but may be used later for reverting if scrapper fails
+//oldFrom = $('#selectedFromCur option:selected').index() + 1;
+//oldTo = $('#selectedToCur option:selected').index() + 1;
+//$('#selectedFromCur').focus(function () {
+//    oldFrom = $('#selectedFromCur').index() + 1;
+//});
+//$('#selectedToCur').focus(function () {
+//    oldFrom = $('#selectedToCur').index() + 1;
+//});
+
+//-------------------------------------------------------------
+
+// Gets current date (currently unused for anything)
+//var currentDate = new Date();
+//var datetime = "Last Sync: " + currentDate.getDate() + "/"
+//  + (currentDate.getMonth() + 1) + "/"
+//  + currentDate.getFullYear() + " @ "
+//  + currentDate.getHours() + ":"
+//  + currentDate.getMinutes() + ":"
+//  + currentDate.getSeconds();
+
+//-------------------------------------------------------------
+
+// If only one cell is given (for some s***** reason it changes to
+// a number type if only one cell is selected, screw consistency
+
+//if (typeof asyncResult.value == "number") {
+//     asyncResult.value = convertValue(parseFloat(asyncResult.value), 2);
+//}
+
+//-------------------------------------------------------------
+
+//if (typeof value !== "number" || isNaN(value)) {
+//   throw new Error("convertValue(): parameter [0] is not of the number type. " +
+//        typeof value + " given (" + value + ").");
+// }
+
+//if (typeof currentExchangeRate !== "number" || isNaN(value)) {
+//   throw new Error("convertValue(): currentExchangeRate is not of the number type. " +
+//       typeof currentExchangeRate + " given (" + currentExchangeRate + ").");
+//app.showNotification(value
