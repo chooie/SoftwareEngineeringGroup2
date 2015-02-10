e1e5acbdc45ec802271b75ddf69a516f8598b604
TomasBarry <tomas.barry@hotmail.com>
2015-02-10 22:43:33 +0000

    Refactoring Step 1
    Added some method header block comments. Changed some variable names
    within convertValue function and abstracted away some repetitive code
    into the validateCurrencyCode function. Also refactored some spacing.

--- CurrencySuiteWeb/App/Home/Home.js
+++ CurrencySuiteWeb/App/Home/Home.js
@@ -3,40 +3,36 @@
 
 (function () {
     "use strict";
-
     var original,
     errorOccurred = false;
-
-    // Handler that is executed when the app is loaded
+    /**
+     * Handler that is executed when the app is loaded
+     * Initializes app, app buttons and app utilities
+     */
     Office.initialize = function () {
         $(document).ready(function () {
             app.initialize();
-            // Initialize the swap button click listener
             $('#swap').click(swap);
-            // Initialize the submit button click listener
             $('#submit').click(convert);
-            // Initialize Scrapper
             scrapperInit();
             datepicker.initialize();
         });
     };
 
-
     /**
+    * scrapperInit
     * Initiates and creates listeners for the scrapper
+    *
     */
     var scrapperInit = function () {
-        currentExchangeRate = 1; //a nice safe value
-        scrapper.initialize(); //initalize the scrapper
-
-
-        //update with the default rate
+        currentExchangeRate = 1; // TODO Remove from final deploy version
+        scrapper.initialize();
         scrapper.updateRate(
             $('#selectedFromCur').val(),
             $('#selectedToCur').val(),
-            scrapper.getDate() //is going to have to be changed when the user can select a date
+            scrapper.getDate()
         );
-        //adding listeners
+        // adding listeners
         $('#selectedFromCur').change(function () {
             scrapper.updateRate(
                 $('#selectedFromCur').val(),
@@ -66,110 +62,136 @@
             )
         });
     }
+
     /**
+     * swap
      * Swaps the selected value in the drop-down
+     *
      */
     var swap = function () {
-        //gets indexes
         var from = $("#selectedFromCur option:selected").index() + 1;
         var to = $("#selectedToCur option:selected").index() + 1;
-
-        //remove the current selections
         $("#selectedToCur :nth-child(" + to + ")").removeAttr('selected');
         $("#selectedFromCur:nth-child(" + from + ")").removeAttr('selected');
-
-        //assign the new selections
         $("#selectedFromCur :nth-child(" + to + ")").prop('selected', true);
         $("#selectedToCur :nth-child(" + from + ")").prop('selected', true);
-   
     };
 
     /**
-     * Determine the exchange rate between two currencies
+     * getExchangeRate
+     * Determine the exchange rate between two currencies for 
+     * the passed date
+     *
      * @param {string} fromCurrency the currency being changed from
      * @param {string} toCurrency the currency being changed to
      * @param {string} date the date to get the rate for
      * @returns {number} The exchange rate based on the parameters
      */
     var getExchangeRate = function (fromCurrency, toCurrency, date) {
-        // TODO
+        // TODO make use of ECB xml docs
         return 10;
     };
 
     /**
-     * Multiplies a number by some exchange rate value.
-     * @param {number} value The initial number
-     * @returns {number} The converted number
+     * validateCurrencyCodes
+     * Determine whether user entered currency codes are valid
+     * Used by the convertValue function to avoid code repitition
+     * 
+     * @param {string} firstCode the from currency code from the user
+     * @param {string} secondCode the to currency code from the user
+     * @return {boolean} true if both codes valid
+     *                   false if one code is not valid
+     */
+     var validateCurrencyCodes = function(firstCode, secondCode) {
+      var optionsDOM = $("option[value]"),
+        fromSelectionIsValid = false,
+        toSelectionIsValid = false;
+      $(optionsDOM).each(function (index, element) {
+        if (element.value === firstCode) {
+          fromSelectionIsValid = true;
+        }
+        if (element.value === secondCode) {
+          toSelectionIsValid = true;
+        }
+      });
+      if (fromSelectionIsValid && toSelectionIsValid) {
+        return true;
+      }
+      return false;
+     };
+
+    /**
+     * convertValue
+     * Get the exchanged value. Can take multiple forms of 
+     * input from the user.
+     *
+     *    Valid Inputs;
+     *
+     *      *****NOTES: 
+     *        Numerical value can contain or omit decimal digits
+     *        Trailing spaces are allowed
+     *        Trailing spaces between values are not allowed
+     *        Leading spaces shouldn't be allowed, need to check
+     *     ******END NOTES
+     *
+     *      - Case 1: "100"
+     *        -> Convert 100 of whatever currency is selected
+     *           in the top drop down bar into whatever
+     *           currency is in the bottom drop down bar based
+     *           on the current date(today)
+     *      - Case 2: "100 USD EUR"
+     *        -> Convert 100 US Dollars into Euros based on
+     *           the current date (today)
+     *      - Case 3: "100 USD EUR "29-02-2004"
+     *        -> Convert 100 US Dollars into Euros based on
+     *           the passed date (29-02-2004 in this case)
+     * TODO discuss implementing "... 29-02" assumes this year
+     * TODO discuss implementing "... 29/02/2004" (different delimeters)
+     *
+     * @param {string} value the user entered query
+     * @returns {number} The exchanged value
      */
     var convertValue = function (value) {
-        try {
-            //^\d = must start with digit, \d = one or more digits, \.? = zero or one decimal point, \d* = zero or more digits
-            // \s+ = one or more spaces, \D{3} = 3 non-digits, \D{3}$ = 3 non-digits, $ means it must end with this
-            if (/^\d+\.?\d*\s+\D{3}\s+\D{3}\s*$/.test(value)) {
-                var valueArray = value.split(" ");
-                //*NOTE* We're going to have to have a delay or something so the API/scrapper can get the exchange rates
-                // need to make sure that input currencies are valid before conversion occurs
-                var optionsDOMFrom = $("option[value]");
-                var selectionFromValid = false;
-                var selectionToValid = false;
-                $(optionsDOMFrom).each(function (index, element) {
-                    if (element.value === valueArray[1].toUpperCase()) {
-                        selectionFromValid = true;
-                    }
-                    if (element.value === valueArray[2].toUpperCase()) {
-                        selectionToValid = true;
-                    }
-                });
-                if (selectionFromValid && selectionToValid) {
-                    return valueArray[0] * getExchangeRate(valueArray[1], valueArray[2], "today");
-                }
-                else {
-                    throw new Error("convertValue(): parameters are wrong. " +
-                        typeof value + " given (" + value + ").");
-                }
-            }
-            else if ((/^\d+\.?\d*\s*$/.test(value))) {
-                //return value * getExchangeRate($('#selectedFromCur').val(), $('#selectedToCur').val(), "today");
-                return value * currentExchangeRate;
-            }
-            //same as above but takes a date in the format dd-mm-yyyy 
-            else if (/^\d+\.?\d*\s+\D{3}\s+\D{3}\s+\d?\d-\d?\d-\d{4}\s*$/.test(value)) {
-                var valueArray = value.split(" ");
-                // return will look something like;
-                // return valueArray[0] * getExchangeRate(value[1], value[2]);
-                //*NOTE* We're going to have to have a delay or something so the API/scrapper can get the exchange rates
-                var optionsDOMFrom = $("option[value]");
-                var selectionFromValid = false;
-                var selectionToValid = false;
-                $(optionsDOMFrom).each(function (index, element) {
-                    if (element.value === valueArray[1].toUpperCase()) {
-                        selectionFromValid = true;
-                    }
-                    if (element.value === valueArray[2].toUpperCase()) {
-                        selectionToValid = true;
-                    }
-                });
-                if (selectionFromValid && selectionToValid) {
-                    return valueArray[0] * getExchangeRate(valueArray[1], valueArray[2], valueArray[3]);
-                }
-                else {
-                    throw new Error("convertValue(): parameters are wrong. " +
-                        typeof value + " given (" + value + ").");
-                }
-            }
-            else {
-                throw new Error("convertValue(): parameters are wrong. " +
-                     typeof value + " given (" + value + ").");
-            }
-            
-        } catch (error) {
-            console.log(error.message);
-            errorOccurred = true;
-            app.showNotification("Warning", "Some data could not be converted as it " +
-                "was not a valid number.");
-            return value;
+      try {
+        // Case 1
+        if (/^\d+\.?\d*\s*$/.test(value)) {
+          return value * getExchangeRate($('#selectedFromCur').val(), $('#selectedToCur').val(), "today");
         }
-
+        // Case 2  
+        else if (/^\d+\.?\d*\s+\D{3}\s+\D{3}\s*$/.test(value)) {
+          var valuesArray = value.split(" ");
+          valuesArray[1] = valuesArray[1].toUpperCase();
+          valuesArray[2] = valuesArray[2].toUpperCase();
+          if (validateCurrencyCodes(valuesArray[1], valuesArray[2])) {
+            return valuesArray[0] * getExchangeRate(valuesArray[1], valuesArray[2], "today");
+          }
+          else {
+            throw new Error("Error throw for Case 2 in convertValue function");
+          }
+        }
+        // Case 3
+        else if (/^\d+\.?\d*\s+\D{3}\s+\D{3}\s+\d?\d-\d?\d-\d{4}\s*$/.test(value)) {
+          var valuesArray = value.split(" ");
+          valuesArray[1] = valuesArray[1].toUpperCase();
+          valuesArray[2] = valuesArray[2].toUpperCase();
+          if (validateCurrencyCodes(valuesArray[1], valuesArray[2])) {
+            return valuesArray[0] * getExchangeRate(valuesArray[1], valuesArray[2], valuesArray[3]);
+          }
+          else {
+            throw new Error("Error throw for Case 3 in convertValue function");
+          }
+        }
+        else {
+          throw new Error("Invalid query entered");
+        }  
+      } 
+      catch (error) {
+        console.log(error.message);
+        errorOccurred = true;
+        app.showNotification("Warning", "Some data could not be converted as it " +
+                             "was not a valid number.");
+        return value;
+      }
     };
 
     /**
