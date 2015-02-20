dc815136e690bbb116e2450699dfdfada01266f3
Charlie Hebert <charlie.hebert92@gmail.com>
2015-02-20 22:49:23 +0000

    Update Currency parser and clean up code

--- CurrencySuiteWeb/App/Currency.js
+++ CurrencySuiteWeb/App/Currency.js
@@ -8,24 +8,20 @@ window.CurrencyConverter = window.CurrencyConverter || {};
 (function($) {
   "use strict";
   $.parser = {
-    urlSourceDaily: "http://www.ecb.europa.eu/stats/" +
-    "eurofxref/eurofxref-daily.xml",
-    urlSourceHistory: undefined,
-    currencyDataDaily: undefined,
-    //currencyDataHistory: this.fetchJSON(this.urlSourceHistory),
+    currencyDataDaily: null,
+    currencyData90Days: null,
+    currencyDataHistorical: null,
 
     /**
-     *
+     * Handles the fetchJSONDaily call via YQL.
      * @param results
      */
-    handleResponse: function(results) {
+    handleResponseDaily: function(results) {
       // Simply populate a div for the time being
       var demo = document.getElementById('demo'),
-        currencies = results["query"]["results"]["Envelope"]["Cube"]["Cube"],
-        jsonCurrencies = JSON.stringify(currencies);
+        //currencies = results["query"]["results"]["Envelope"]["Cube"]["Cube"],
+        jsonCurrencies = JSON.stringify(results);
 
-      // TODO fix this
-      // for some reason, 'this' refers to the prototype object
       $.parser.currencyDataDaily = jsonCurrencies;
 
       demo.innerHTML = (results.error ?
@@ -33,22 +29,72 @@ window.CurrencyConverter = window.CurrencyConverter || {};
     },
 
     /**
-     *
-     * @param url
+     * Handles the fetchJSON90Days call via YQL.
+     * @param results
      */
-    fetchJSON: function(url) {
-      var root = 'https://query.yahooapis.com/v1/public/yql?q=',
+    handleResponse90Days: function(results) {
+      $.parser.currencyData90Days = JSON.stringify(results);
+
+    },
+
+    /**
+     * Handles the fetchJSONHistorical call via YQL.
+     * @param results
+     */
+    handleResponseHistorical: function(results) {
+      $.parser.currencyData90Days = JSON.stringify(results);
+    },
+
+    /**
+     * Uses YQL to get the most up to date currency data for the day.
+     */
+    fetchJSONDaily: function() {
+      var url = "http://www.ecb.europa.eu/stats/" +
+          "eurofxref/eurofxref-daily.xml",
+        root = 'https://query.yahooapis.com/v1/public/yql?q=',
         yql = 'select * from xml where url="' + url + '"',
         proxyUrl = root + encodeURIComponent(yql) +
           "&format=json&diagnostics=false&" +
-          "callback=CurrencyConverter.Currency.parser.handleResponse",
+          "callback=CurrencyConverter.Currency.parser.handleResponseDaily",
         body = document.getElementsByTagName('body')[0];
 
       body.appendChild(this.createScriptNode(proxyUrl));
     },
 
     /**
-     *
+     * Uses YQL to get the currency data for the last 90 days.
+     */
+    fetchJSON90Days: function() {
+      var url = "http://www.ecb.europa.eu/stats/eurofxref/" +
+          "eurofxref-hist-90d.xml",
+        root = 'https://query.yahooapis.com/v1/public/yql?q=',
+        yql = 'select * from xml where url="' + url + '"',
+        proxyUrl = root + encodeURIComponent(yql) +
+          "&format=json&diagnostics=false&" +
+          "callback=CurrencyConverter.Currency.parser.handleResponse90Days",
+        body = document.getElementsByTagName('body')[0];
+
+      body.appendChild(this.createScriptNode(proxyUrl));
+    },
+
+    /**
+     * Uses YQL to get the historical currency data (going back to 1999).
+     */
+    fetchJSONHistorical: function() {
+      var url = "http://www.ecb.europa.eu/stats/eurofxref/" +
+          "eurofxref-hist.xml",
+        root = 'https://query.yahooapis.com/v1/public/yql?q=',
+        yql = 'select * from xml where url="' + url + '"',
+        proxyUrl = root + encodeURIComponent(yql) +
+          "&format=json&diagnostics=false&" +
+          "callback=CurrencyConverter.Currency.parser.handleResponseHistorical",
+        body = document.getElementsByTagName('body')[0];
+
+      body.appendChild(this.createScriptNode(proxyUrl));
+    },
+
+    /**
+     * Create a script node with a src attribute to some URL.
      * @param url
      * @returns {HTMLElement}
      */
@@ -60,22 +106,30 @@ window.CurrencyConverter = window.CurrencyConverter || {};
     },
 
     /**
-     *
-     * @returns {*}
+     * @returns {JSON}
      */
     getDailyCurrencyData: function() {
-      //var timeoutID = setTimeout(function() {
-      //  throw new Error("Server took too long.");
-      //}, 5000);
-      var that = this;
+      return this.currencyDataDaily;
+    },
 
-      if (typeof this.currencyDataDaily !== "object") {
-        // Poll every second
-        setTimeout(that.getDailyCurrencyData, 1000);
-      } else {
-        return this.currencyDataDaily
-      }
+    /**
+     * @returns {JSON}
+     */
+    get90DaysCurrencyData: function() {
+      return this.currencyData90Days;
+    },
+
+    /**
+     * @returns {JSON}
+     */
+    getHistoricalCurrencyData: function() {
+      return this.currencyDataHistorical;
     }
   };
-  $.parser.fetchJSON($.parser.urlSourceDaily);
+  $.parser.fetchJSONDaily();
+  $.parser.fetchJSON90Days();
+  // TODO fetchJSONHistorical is returning null - need to fix this.
+  // Probably an issue with YQL having to parse too much data.
+  // Should set some sort of constraint on what data it needs to parse.
+  //$.parser.fetchJSONHistorical();
 }(window.CurrencyConverter.Currency = window.CurrencyConverter.Currency || {}));
