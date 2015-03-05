d1ba368969eec1f79c8a2caeb8edcafd91c11d2c
Charlie Hebert <charlie.hebert92@gmail.com>
2015-03-05 20:16:12 +0000

    Remove unused variables and methods. Refactor with linter

--- CurrencySuiteWeb/App/Graph/Graph.js
+++ CurrencySuiteWeb/App/Graph/Graph.js
@@ -1,68 +1,70 @@
 ﻿window.CurrencyConverter = window.CurrencyConverter || {};
 window.CurrencyConverter.graph = (function () {
   var updateGraph,
-    initialize,
-    highlightDataPoint,
-    graph,
-    numberOfTries = 10;
+    initialize;
 
   updateGraph = function(values) {
-    //if (graphItem != null) {
-    //graphItem.setData(values);
-    //return;
-    //}
-    if (values == null) {
-      return;
-    }
-    var date = window.CurrencyConverter.datepicker.getSelectedDate();
-    var day = [];
-    for (var i = 0; i < values.length; i++) {
-      if (date.valueOf() === values[i][0].valueOf()) {
-        day.push(values[i]);
-      }
-
-    }
+    
     var fromCur = $('#from-currency').val(),
       toCur = $('#to-currency').val(),
-      graphData;
-
-    $('#graph-label').text(fromCur + " to " + toCur);
-    // Graph Data ##############################################
-    graphData = [{
-      // [Date, Value] 2010/08/17
-      data: values,
-      color: '#71c73e'
-    },
-    {
-      data: day,
-      color: '#2a8dd4'
-    }];
-    // Lines Graph #############################################
-    graph = $.plot($('#graph-lines'), graphData, {
-      series: {
-        points: {
-          show: true,
-          radius: 5
-        },
-        lines: {
-          show: true
-        },
-        shadowSize: 0
-      },
-      grid: {
-        color: '#646464',
-        borderColor: 'transparent',
-        borderWidth: 20,
-        hoverable: true
-      },
-      xaxis: {
-        tickColor: 'transparent',
-        mode: "time",
-        timeformat: "%d/%m"
-      },
-      yaxis: {}
-    });
+      graphData,
+      date = window.CurrencyConverter.datepicker.getSelectedDate(),
+      day = [],
+      i;
+    try {
+      if (! Array.isArray(values)) {
+        throw new Error("updateGraph(): Paramater [0] must be an instance of " +
+          "Array.");
+      }
     
+      for (i = 0; i < values.length; i++) {
+        if (date.valueOf() === values[i][0].valueOf()) {
+          day.push(values[i]);
+        }
+      }
+   
+      $('#graph-label').text(fromCur + " to " + toCur);
+
+      // Graph Data ##############################################
+      graphData = [{
+        // [Date, Value] 2010/08/17
+        data: values,
+        color: '#71c73e'
+      },
+      {
+        data: day,
+        color: '#2a8dd4'
+      }];
+
+      // Lines Graph #############################################
+      $.plot($('#graph-lines'), graphData, {
+        series: {
+          points: {
+            show: true,
+            radius: 5
+          },
+          lines: {
+            show: true
+          },
+          shadowSize: 0
+        },
+        grid: {
+          color: '#646464',
+          borderColor: 'transparent',
+          borderWidth: 20,
+          hoverable: true
+        },
+        xaxis: {
+          tickColor: 'transparent',
+          mode: "time",
+          timeformat: "%d/%m"
+        },
+        yaxis: {}
+      });
+    } catch (e) {
+      console.log("Exception: " + e.name + " - " + e.message);
+      return;
+    }
   };
 
   // Common initialization function (to be called from each page)
@@ -112,39 +114,8 @@ window.CurrencyConverter.graph = (function () {
     );
   };
 
-  highlightDataPoint = function () {
-    // TODO
-    var selectedDate = "03-03-2015",//$("#datepicker").val(),
-      correctFormat = window.CurrencyConverter.datepicker.correctFormat;
-    setTimeout(function () {
-      try {
-        if (!Array.isArray(graph.getData()[0].data[0])) {
-          throw new Error("Data didn't load in time");
-        }
-
-        graph.getData()[0].data.forEach(function (dataPoint) {
-          if (correctFormat(new Date(dataPoint[0])) === selectedDate) {
-            // DO SOMETHING TO THE DATA POINT THAT MATCHES THE SELECTED DATE
-            // i.e. give it a different colour
-            console.log("Found it");
-          }
-        });
-        numberOfTries = 10;
-      } catch (e) {
-        console.log(e);
-        numberOfTries -= 1;
-        if (numberOfTries <= 0) {
-          throw new Error("Couldn't get graph data after a number of tries.");
-        }
-        highlightDataPoint();
-      }
-    }, 5000);
-
-  };
-
   return {
     initialize: initialize,
-    update: updateGraph,
-    highlightDataPoint: highlightDataPoint
+    update: updateGraph
   };
 }());
